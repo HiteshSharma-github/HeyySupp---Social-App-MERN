5a19fcdd872986a028a1fdf61c993b014f9ceced
Hitesh Sharma <hiteshsharmahs0987@gmail.com>
2022-06-05 22:09:03 +0530

    first commit

---
+++ client/theme.js
@@ -0,0 +1,24 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import { teal, orange } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+      light: '#BC1CEC',
+      main: '#4972DC',
+      dark: '#E6B571',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffd95b',
+      main: '#ffa726',
+      dark: '#c77800',
+      contrastText: '#000',
+    },
+      openTitle: teal['700'],
+      protectedTitle: orange['700'],
+      type: 'light'
+    }
+  })
+
+  export default theme  
